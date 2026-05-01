7f4944dab577943d9c424a3bcc38f671fc0074bc
CoPanel Dev <dev@copanel.local>
2026-05-01 16:06:53 +0700

    Fix gitignore pattern and add missing utils.ts file

---
+++ frontend/src/lib/utils.ts
@@ -0,0 +1,9 @@
+/**
+ * Tailwind CSS utilities
+ */
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
